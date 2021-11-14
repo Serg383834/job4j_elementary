da0b24f00025d0d4edfc364972ab4a3619238d34
Serg383834 <71527273+Serg383834@users.noreply.github.com>
2021-11-14 18:03:08 +0300

    Update MultipleSwitchWeek.java

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -1,9 +1,10 @@
 package ru.job4j.condition;
 
 public class MultipleSwitchWeek {
+    
     public static int numberOfDay(String name) {
         return switch (name) {
-            case "Понедельник", "Monday" -> 1;
+            case "Понедельник", "Monday" -> 1;https://github.com/Serg383834/job4j_elementary/blob/master/src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
             case "Вторник", "Tuesday" -> 2;
             case "Среда", "Wednesday" -> 3;
             case "Четверг", "Thursday" -> 4;
