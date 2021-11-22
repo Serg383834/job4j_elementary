536e43ad2cede6ac9424eb96cf9b5f8aa2140ccf
Serg383834 <serg383834@yandex.ru>
2021-11-22 09:06:16 +0300

    6.0.1. Объявление массива. [#3613 #9669]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,10 @@
+package ru.job4j.array;
+// Внутри метода main создайте три переменных.
+//         - Массив на 10 элементов типа short с именем ages.
+//         - Массив на 100500 элементов типа String c именем surnames.
+//         - Массив на 40 элементов типа float с именем prices.
+public class ArrayDefinition {
+    short[] ages = new short[10];
+    String[] surnames = new String[100500];
+    float[] prices = new float[40];
+}
